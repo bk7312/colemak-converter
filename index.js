2f3b24b0fb4943553ae564f87c6d0635f44c71e6
bk7312 <bk7312@users.noreply.github.com>
2023-12-13 14:46:33 +0800

    added new copy feature
    hidden keyboard shortcut => ctrl + 'C' (i.e. shift + 'c')

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 
-
+const $html = document.querySelector('html')
+const $banner = document.querySelector('#banner')
 const $qwerty = document.querySelector('#qwerty')
 const $colemak = document.querySelector('#colemak')
+const $qwertyCopyBtn = document.querySelector('#copy-qwerty')
+const $colemakCopyBtn = document.querySelector('#copy-colemak')
 
+$html.addEventListener('keydown', keyboardCopy)
 $qwerty.addEventListener('input', () => toColemak())
 $colemak.addEventListener('input', () => toQwerty())
+$qwertyCopyBtn.addEventListener('click', () => copyText($qwerty.value))
+$colemakCopyBtn.addEventListener('click', () => copyText($colemak.value))
 
-const qwertyStr  = "qwertyuiopasdfghjkl;zxcvbnmQWERTYUIOPASDFGHJKL:ZXCVBNM"
+const qwertyStr = "qwertyuiopasdfghjkl;zxcvbnmQWERTYUIOPASDFGHJKL:ZXCVBNM"
 const colemakStr = "qwfpgjluy;arstdhneiozxcvbkmQWFPGJLUY:ARSTDHNEIOZXCVBKM"
 
 function toColemak() {
@@ -19,9 +25,41 @@ function toQwerty() {
 
 function convert(str, strTo, strFrom) {
     let res = ""
-    for(let char of str) {
+    for (let char of str) {
         const index = strFrom.indexOf(char)
         res += index >= 0 ? strTo[index] : char
     }
     return res
 }
+
+async function copyText(str) {
+    const text = str.trim()
+    if (text === '') {
+        showBanner('Nothing to copy...', 'notOk')
+        return
+    }
+    await navigator.clipboard.writeText(text)
+    const bannerText = text.length < 50 ? text : text.slice(0, 50).trim() + '...'
+    showBanner(`"${bannerText}" copied to clipboard!`, 'ok')
+}
+
+async function keyboardCopy(e) {
+    if (e.ctrlKey && e.key === 'C') {
+        const target = e.target.id === 'qwerty' ?
+            $colemak.value
+            : e.target.id === 'colemak' ?
+                $qwerty.value : ''
+        copyText(target)
+    }
+}
+
+function showBanner(str, type = 'ok') {
+    $banner.textContent = str
+    $banner.style.background = type === 'ok' ? 'lime' : 'salmon'
+    $banner.classList.add('open')
+    setTimeout(() => {
+        $banner.classList.remove('open')
+    }, 1500)
+}
+
+$qwerty.focus()
