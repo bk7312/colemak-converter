7f2e85be71cdf4c30551714088c2ada3753b96b0
bk7312 <bk7312@users.noreply.github.com>
2023-12-13 21:08:04 +0800

    general improvements
    - changed hidden keyboard shortcut to ctrl+alt+shift
    - shorten search param keys
    - added try/catch block to navigator.clipboard
    - improved banner

--- index.js
+++ index.js
@@ -1,32 +1,24 @@
 
 const $html = document.querySelector('html')
 const $banner = document.querySelector('#banner')
+const $bannerText = document.querySelector('#banner-text')
+const $closeBannerBtn = document.querySelector('#close-banner')
 const $qwerty = document.querySelector('#qwerty')
 const $colemak = document.querySelector('#colemak')
 const $qwertyCopyBtn = document.querySelector('#copy-qwerty')
 const $colemakCopyBtn = document.querySelector('#copy-colemak')
 
 $html.addEventListener('keydown', keyboardCopy)
-$qwerty.addEventListener('input', () => toColemak())
-$colemak.addEventListener('input', () => toQwerty())
+$qwerty.addEventListener('input', toColemak)
+$colemak.addEventListener('input', toQwerty)
 $qwertyCopyBtn.addEventListener('click', () => copyText($qwerty.value))
 $colemakCopyBtn.addEventListener('click', () => copyText($colemak.value))
+$closeBannerBtn.addEventListener('click', closeBanner)
 
 const qwertyStr = "qwertyuiopasdfghjkl;zxcvbnmQWERTYUIOPASDFGHJKL:ZXCVBNM"
 const colemakStr = "qwfpgjluy;arstdhneiozxcvbkmQWFPGJLUY:ARSTDHNEIOZXCVBKM"
 
-const url = new URL(window.location.href)
-const params = new URLSearchParams(url.search)
-
-if (params.has('qtc')) {
-    $qwerty.value = params.get('qtc')
-    toColemak()
-    copyText($colemak.value)
-} else if (params.has('ctq')) {
-    $colemak.value = params.get('ctq')
-    toQwerty()
-    copyText($qwerty.value)
-}
+let bannerTimeout
 
 function toColemak() {
     $colemak.value = convert($qwerty.value, colemakStr, qwertyStr)
@@ -51,13 +43,17 @@ async function copyText(str) {
         showBanner('Nothing to copy...', 'notOk')
         return
     }
-    await navigator.clipboard.writeText(text)
-    const bannerText = text.length < 50 ? text : text.slice(0, 50).trim() + '...'
-    showBanner(`"${bannerText}" copied to clipboard!`, 'ok')
+    try {
+        await navigator.clipboard.writeText(text)
+        const bannerText = text.length < 50 ? text : text.slice(0, 50).trim() + '...'
+        showBanner(`"${bannerText}" copied to clipboard!`, 'ok')
+    } catch (e) {
+        showBanner(`${e.name}: ${e.message}`, 'notOk')
+    }
 }
 
 async function keyboardCopy(e) {
-    if (e.ctrlKey && e.key === 'C') {
+    if (e.ctrlKey && e.shiftKey && e.altKey) {
         const target = e.target.id === 'qwerty' ?
             $colemak.value
             : e.target.id === 'colemak' ?
@@ -67,12 +63,31 @@ async function keyboardCopy(e) {
 }
 
 function showBanner(str, type = 'ok') {
-    $banner.textContent = str
+    clearTimeout(bannerTimeout)
+    $bannerText.textContent = str
     $banner.style.background = type === 'ok' ? 'lime' : 'salmon'
-    $banner.classList.add('open')
-    setTimeout(() => {
-        $banner.classList.remove('open')
-    }, 1500)
+    $banner.classList.remove('hide')
+    $closeBannerBtn.classList.remove('hide')
+    bannerTimeout = setTimeout(() => closeBanner(), 3000)
 }
 
-$qwerty.focus()
+function closeBanner() {
+    $banner.classList.add('hide')
+}
+
+const url = new URL(window.location.href)
+const params = new URLSearchParams(url.search)
+
+if (params.has('q')) {
+    $qwerty.value = params.get('q')
+    toColemak()
+    $qwerty.focus()
+    copyText($colemak.value)
+} else if (params.has('c')) {
+    $colemak.value = params.get('c')
+    toQwerty()
+    $colemak.focus()
+    copyText($qwerty.value)
+} else {
+    $qwerty.focus()
+}
